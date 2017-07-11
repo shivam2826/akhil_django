3655e1389f7807b6282c7674811cd8074863133f
Shivam Maurya <shivam.m2826@gmail.com>
2017-07-11 05:04:08 +0000

    via HttpResponse

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 def index(request):
-    #return HttpResponse()
-    return render(request, "personal/home.html")
+    #now returning HttpResponse()
+    #return render(request, "personal/home.html")
+    temp=loader.get_template("personal/home.html")
+    return HttpResponse(temp.render({},request))
